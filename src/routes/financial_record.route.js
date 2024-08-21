f886f5d95f9b3e6650a9831e3d7bf00e00eef1ef
LuizMartinsDev <luizkwdev@gmail.com>
2024-08-21 03:27:12 -0300

    feat(registro): atualiação de registro

--- src/routes/financial_record.route.js
+++ src/routes/financial_record.route.js
@@ -3,6 +3,7 @@ const financialRecord = require('../controller/financial_record.controller')
 
 route.post('/', financialRecord.create);
 route.get('/', financialRecord.findAll);
-route.get('/:id', financialRecord.findById)
+route.get('/:id', financialRecord.findById);
+route.patch('/:id', financialRecord.update)
 
 module.exports = route
--- src/services/financial_record.service.js
+++ src/services/financial_record.service.js
@@ -3,9 +3,17 @@ const Record = require('../models/Record');
 const createRecord = (body) => Record.create(body);
 const findAllRecord = () => Record.find();
 const findById = (id) => Record.findById(id);
+const update = (
+    id, 
+    description,
+    registration_date,
+    type,
+    registration_value,
+    registration_status) => Record.findByIdAndUpdate({_id: id}, { description, registration_date, type, registration_value, registration_status})
 
 module.exports = {
     createRecord,
     findAllRecord,
-    findById
+    findById,
+    update
 }
