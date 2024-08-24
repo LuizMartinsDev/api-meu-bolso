f612a03cf22a5c01253cee2b19a07b626656f1f6
LuizMartinsDev <luizkwdev@gmail.com>
2024-08-24 15:18:43 -0300

    feat(registro): criação de middlewares para validar id e record

--- src/routes/financial_record.route.js
+++ src/routes/financial_record.route.js
@@ -1,10 +1,11 @@
 const route = require('express').Router();
-const financialRecord = require('../controller/financial_record.controller')
+const financialRecord = require('../controller/financial_record.controller');
+const {validId, validRecord} = require('../middlewares/global.middlewares')
 
 route.post('/', financialRecord.create);
 route.get('/', financialRecord.findAll);
-route.get('/:id', financialRecord.findById);
-route.patch('/:id', financialRecord.update);
-route.delete('/:id', financialRecord.deleteRecord)
+route.get('/:id', validId, validRecord,  financialRecord.findById);
+route.patch('/:id', validId, validRecord,  financialRecord.update);
+route.delete('/:id', validId, validRecord,  financialRecord.deleteRecord)
 
 module.exports = route
